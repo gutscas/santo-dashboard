03cc250e2481d22114dbe9d44a7e95ffd695fe4e
gutscas <santhoshkalangiyam@gmail.com>
2025-11-26 18:35:02 +0530

    santo19

--- myproject/settings.py
+++ myproject/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'santo-project', 'dist', 'assets'),  # use your actual dist folder
+    BASE_DIR / "santo-project" / "dist"
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # collectstatic output

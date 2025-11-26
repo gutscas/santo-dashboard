5d8c7b28d716a567285e0cc4a58951ee692fee7b
gutscas <santhoshkalangiyam@gmail.com>
2025-11-26 19:07:44 +0530

    santo22

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-restoration-key-replace-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['210.18.176.178']
 
 # Application definition
 

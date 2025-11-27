66cb8408bebab89f851ec96ae445b65ac5936f29
gutscas <santhoshkalangiyam@gmail.com>
2025-11-27 10:25:48 +0530

    santo23

--- santo-project/src/services/apiReusable.js
+++ santo-project/src/services/apiReusable.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://project.rayi.in/api/",
     headers: { "Content-Type": "application/json" },
 });
 

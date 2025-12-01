79eecf83ff61fe2b914fa17c8c45cefdf1512580
gutscas <santhoshkalangiyam@gmail.com>
2025-12-01 16:04:22 +0530

    api resolve

--- santo-project/src/services/apiReusable.js
+++ santo-project/src/services/apiReusable.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://project.rayi.in/",
+    baseURL: "https://project.rayi.in/dashboard-api",
     headers: { "Content-Type": "application/json" },
 });
 
